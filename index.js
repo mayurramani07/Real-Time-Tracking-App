526908354c5fc48f68e5f03f935e4db45e54918c
mayurramani07 <ramanimayur836@gmail.com>
2024-12-25 01:06:03 +0530

    Message

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
+const http = require("http");
 
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+app.set("view engine" , "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/" , function (req, res) {
+    res.send("hey");
+});
+
+const port = 7001;
+app.listen(port , () => {
+    console.log(`server is running on port ${port}`);
+});
